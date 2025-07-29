6a474c03cad07ef398ae04c5a31a848ed359c576
Erick J. Gutierrez H. <erick.gutierrez@gruporizek.local>
2025-07-29 17:27:51 -0400

    Subiendo avances mapa

--- src/app/cines/crear-cine/crear-cine.component.ts
+++ src/app/cines/crear-cine/crear-cine.component.ts
@@ -1,17 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { CineCreacionDTO } from '../cines';
+import { FormularioCinesComponent } from "../formulario-cines/formulario-cines.component";
 
 @Component({
   selector: 'app-crear-cine',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, FormularioCinesComponent],
   templateUrl: './crear-cine.component.html',
   styleUrl: './crear-cine.component.css'
 })
 export class CrearCineComponent {
-  router = inject(Router);
+  
 
-   guardarCambios(){
-    this.router.navigate(['/cines']);
+   guardarCambios(cine: CineCreacionDTO){
+      console.log('creando cine',cine);
    }
 }
---
+++ src/app/compartidos/componentes/mapa/mapa.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { icon, latLng, LeafletMouseEvent, marker, Marker, MarkerOptions, tileLayer } from 'leaflet';
+import {LeafletModule} from '@bluehalo/ngx-leaflet'
+
+@Component({
+  selector: 'app-mapa',
+  imports: [LeafletModule],
+  templateUrl: './mapa.component.html',
+  styleUrl: './mapa.component.css'
+})
+export class MapaComponent {
+
+  markerOptions: MarkerOptions = {
+    icon: icon({
+      iconSize:[25,41],
+      iconAnchor: [13,41],
+      iconUrl: 'assets/market-icon.png',
+      iconRetinaUrl: 'assets/market-icon-2x.png',
+      shadowUrl: 'assets/market-shadow.png'
+    })
+  }
+
+
+  // markerOptions: MarkerOptions = {
+  //   icon: icon({
+  //     iconSize:[25,41],
+  //     iconAnchor: [13,41],
+  //     iconUrl: 'assets/market-icon.png',
+  //     iconRetinaUrl: 'assets/market-icon-2x.png',
+  //     shadowUrl: 'assets/market-shadow.png'
+  //   })
+  // }
+
+options = {
+  layers: [
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    maxZoom: 18,
+    attribution: '...'
+    })
+  ],
+  zoom: 14,
+  center: latLng(19.28996410846533,-70.26237291041258)
+}
+
+capas: Marker<any>[]=[];
+
+manejarClick(event: LeafletMouseEvent){
+  const latitud  = event.latlng.lat;
+  const longitud = event.latlng.lng;
+
+  this.capas = [];
+  this.capas.push(marker([latitud,longitud],this.markerOptions));
+}
+
+}
